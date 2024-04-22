9eafe2a33a62fa0016191c2b86e062a79ea6b269
Randall Adams <ml.at.scale.randy@gmail.com>
2024-04-21 22:16:10 -0400

    wip: function signature with default values

--- db/src/functions/assoc_get.sql
+++ db/src/functions/assoc_get.sql
@@ -2,9 +2,11 @@
 -- Get the associations
 --
 CREATE OR REPLACE FUNCTION assoc_get(
-    id1 NUMERIC
-  , atype VARCHAR(50)
-  , id2 NUMERIC
+    id1        NUMERIC
+  , atype      VARCHAR(50)
+  , id2        NUMERIC
+  , low_tstz   TIMESTAMP WITH TIME ZONE DEFAULT '1970-01-01T00:00:00-0'
+  , high_tstz  TIMESTAMP WITH TIME ZONE DEFAULT '1970-01-01T00:00:00-0'
 ) RETURNS VOID AS $$
 DECLARE
 BEGIN
